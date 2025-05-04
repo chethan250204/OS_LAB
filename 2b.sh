4fbbfdbc33baaece84df8d2e977bb6504020cf53
Chethan S Narayan <163844403+chethan250204@users.noreply.github.com>
2025-05-04 07:20:37 +0530

    Create 2b.sh

---
+++ 2b.sh
@@ -0,0 +1,16 @@
+echo "Enter two numbers"
+read n1
+read n2
+echo "enter operator"
+read op
+case $op in
+'+') echo "$n1+$n2=$(($n1+$n2))"
+;;
+'-') echo "$n1-$n2=$(($n1-$n2))"
+;;
+'*') echo "$n1*$n2=$((n1*n2))"
+;;
+'/') echo "$n1/$n2=$(($n1/$n2))"
+;;
+*) echo "Invalid"
+esac
